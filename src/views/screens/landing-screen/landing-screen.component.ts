9e65a6898a92ddd7a2782249bc364891068d08e7
Nils <nils.verneret@gmail.com>
2025-03-30 20:21:51 +0200

    Initial commit

---
+++ src/views/screens/landing-screen/landing-screen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-screen',
+  imports: [],
+  templateUrl: './landing-screen.component.html',
+  styleUrl: './landing-screen.component.scss'
+})
+export class LandingScreenComponent {
+
+}
