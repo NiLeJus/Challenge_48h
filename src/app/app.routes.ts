bfac376c72e7a49c2f3ed5dfc4e2887f82a1979f
Epikdimond-N <nathanael.pivot@ynov.com>
2025-03-31 14:37:48 +0200

    feat(routing): landing-screen routing ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LandingScreenComponent } from '../views/screens/landing-screen/landing-screen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LandingScreenComponent },
+    { path: '**', component: LandingScreenComponent }
+];
