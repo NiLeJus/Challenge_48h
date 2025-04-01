5fa00af21210f42e6d6dcf8ee5eb87356d341604
Epikdimond-N <nathanael.pivot@ynov.com>
2025-04-01 08:41:08 +0200

    feat (runner): runner ok without end button

--- src/views/screens/landing-screen/landing-screen.component.ts
+++ src/views/screens/landing-screen/landing-screen.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CorrectCssComponent } from "../correct-css/correct-css.component";
+import { RunnerComponent } from '../runner/runner.component';
 
 @Component({
   selector: 'app-landing-screen',
-  imports: [CorrectCssComponent],
+  imports: [RunnerComponent, CorrectCssComponent],
   templateUrl: './landing-screen.component.html',
   styleUrl: './landing-screen.component.scss'
 })
